4f71fc75ee5b9e0f087a96a6579940f661301086
himagx4 <82393700+himagx4@users.noreply.github.com>
2025-06-29 05:06:32 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,9 @@
+import re
+
+def scan_text(text):
+    leaks = []
+    emails = re.findall(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", text)
+    if emails:
+        leaks.extend(emails)
+    return leaks
 
